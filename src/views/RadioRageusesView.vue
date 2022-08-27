02d7b303f69efa9028f4cf60931f915e978f5f19
Vivi <vachetvirginie@gmail.com>
2022-08-27 16:46:54 +0200

    first commit

---
+++ src/views/RadioRageusesView.vue
@@ -0,0 +1,25 @@
+<template>
+  <vue-iframe
+    :src="src"
+    frame-id="my-ifram"
+    @load="onLoad"
+    name="my-frame"
+    width="150px"
+    height="2000px"
+  />
+</template>
+
+<script>
+export default {
+  name: "MyIframe",
+  data: () => ({
+    myIframe: null,
+    src: "https://rebellyon.info/",
+  }),
+  methods: {
+    onLoad(frame) {
+      this.myIframe = frame.contentWindow;
+    },
+  },
+};
+</script>
