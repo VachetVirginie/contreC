88dfdf836821592c6970ae5441783e53b33ac78b
Vivi <vachetvirginie@gmail.com>
2022-08-27 23:28:59 +0200

    wip pwa min config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    name: "Contre C",
+    themeColor: "#FFFFFF",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    // workboxOptions: {
+    //   skipWaiting: true,
+    // },
+    // configure the manifest options
+    manifestOptions: {
+      display: "landscape",
+      background_color: "#FFFFFF",
+      // ...
+    },
+  },
 });
