431b849f74eb7b8c87cda71bae25eb693743734c
Vivi <vachetvirginie@gmail.com>
2022-08-28 16:21:17 +0200

    add dynamic active menu

--- src/components/BottomNavigationComponent.vue
+++ src/components/BottomNavigationComponent.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="btm-nav gap-2 overflow-auto scrollbar-hide">
-    <button v-for="item in menu" :key="item">
+    <button
+      v-for="(item, index) in menu"
+      :key="item"
+      :class="isActive === index ? 'active text-primary' : ''"
+      @click="showIndex(index)"
+    >
       <span class="text-white">
         <router-link :to="item.link">{{ item.name }}</router-link>
       </span>
@@ -12,6 +17,7 @@
 export default {
   name: "BottomComponent",
   data: () => ({
+    isActive: 0,
     menu: [
       { link: "/", name: "🏠" },
       { link: "/blast", name: "🌬️" },
@@ -21,5 +27,10 @@ export default {
       { link: "/ideeculture", name: "💡" },
     ],
   }),
+  methods: {
+    showIndex(index) {
+      this.isActive = index;
+    },
+  },
 };
 </script>
